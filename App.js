c59dd4d0c4cb9c99c57657931d626d94aab65470
amits7740 <30660803+amits7740@users.noreply.github.com>
2019-12-05 16:42:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+   
+   <div class="App-header">
+<h1 class="App-link">Team: <span>Smart learner</span></h1>
+
+<h4>Anamika Chatterjee (Coach) </h4>
+<h4>Sayantan Roy (Captain)</h4>
+<h4>Hara Jena</h4>
+<h4>Siri Chandana</h4>
+<h4>Amit Singh</h4>
+<h4>Abhipsa</h4>
+
+
+   </div>
+       
+  );
+}
+
+export default App;
