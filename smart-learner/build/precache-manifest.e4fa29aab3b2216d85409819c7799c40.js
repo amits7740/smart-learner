990734296ea627811ad5136043470d445bee9c64
Siri <siri.chandu2597@gmail.com>
2019-12-19 16:11:25 +0530

    build

---
+++ smart-learner/build/precache-manifest.e4fa29aab3b2216d85409819c7799c40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1248e1db69edf2869a616f65bca68c6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8aed9b6e8e451ca000c",
+    "url": "/static/css/2.ffff300b.chunk.css"
+  },
+  {
+    "revision": "a2d2fefeeaa89fdfa29a",
+    "url": "/static/css/main.d86f3866.chunk.css"
+  },
+  {
+    "revision": "b8aed9b6e8e451ca000c",
+    "url": "/static/js/2.66abb008.chunk.js"
+  },
+  {
+    "revision": "a2d2fefeeaa89fdfa29a",
+    "url": "/static/js/main.88761bb1.chunk.js"
+  },
+  {
+    "revision": "1040a848f68b9d0a4acf",
+    "url": "/static/js/runtime-main.315daa09.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
